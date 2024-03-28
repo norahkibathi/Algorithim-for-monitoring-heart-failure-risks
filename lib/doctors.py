6ff6b13b3e2fde9a02c785279b82e906f64c1ace
norahkibathi <norah.njeri@student.moringaschool.com>
2024-03-28 09:22:27 +0300

    almost there

--- lib/doctors.py
+++ lib/doctors.py
@@ -11,17 +11,23 @@ class Doctor(Base):
   lastname = Column("Lastname",String)
   age = Column("Age", Integer)
   gender = Column("Gender",String)
+  email = Column("Email", String) 
+  address = Column("Address", String)  
+  phone_number = Column("Phone Number", String)  
   working_hours = Column("Working hours",Integer)
   salary = Column("Salary", Float, nullable=False)
   #this is a one to many relationship where a single doctors expert can have many patients
   patients = relationship("Patient", backref="doctor")
 
-  def __init__(self,id,firstname, lastname, gender,age,salary,working_hours=0):
+  def __init__(self,id,firstname, lastname, gender,age,address,email, phone_number,salary,working_hours=0):
     self.id = None  
     self.firstname = firstname 
     self.lastname = lastname
     self.age = age
     self.gender = gender
+    self.address =address
+    self.email= email
+    self.phone_number = phone_number
     self.working_hours = working_hours
     self.salary = salary 
     self.calculate_salary()
@@ -32,7 +38,7 @@ class Doctor(Base):
 
   def __str__(self):
    #template for printing doctors details 
-       return f"Doctor:ID: {self.id}, {self.first_name} {self.last_name} (ID: {self.id}) - Age: {self.age}, Gender: {self.gender}, Working Hours: {self.working_hours}, Salary: ${self.salary:.2f}"
+       return f"fitness Expert:ID: {self.id}, {self.first_name} {self.last_name} Age: {self.age}, Gender: {self.gender},Address:{self.address}, Email: {self.email} , Phone_number: {self.phone_number}, Working Hours: {self.working_hours}, Salary: ${self.salary:.2f}"
 
 #example
-  #doctor1 = Doctor("Douglous", "Wambai", 39, "Male", 90000, 90)
+  #doctor1 = Doctor("Douglous", "Wambai", 39, "Male", "gathage", "dagy@gmail", 07224455, 90000, 90)
--- lib/fitness.py
+++ lib/fitness.py
@@ -5,19 +5,35 @@ from base import Base
 class FitnessExpert(Base):
   __tablename__ = 'fitness_experts'
 
-  id = Column("id",Integer, primary_key=True)
-  firstname = Column("Firstname",String)
-  lastname = Column("Lastname",String)
+  
+  id = Column(Integer, primary_key=True)
+  firstname = Column("Firstname", String)
+  lastname = Column("Lastname", String)
   age = Column("Age", Integer)
-  gender = Column("Gender",String)
-  working_hours = Column("Working hours",Integer)
-  salary = Column("Salary", Float, nullable=False)
-  specialization = Column("specialization", String(50))  
-  working_hours = Column("working_hours", Integer)
+  gender = Column("Gender", String)
+  working_hours = Column("Working hours", Integer)  
+  salary = Column("Salary", Float)  
+  specialization = Column("specialization", String(50))
   #this is a one to many relationship where a single fitness expert can have many patients
   patients = relationship("Patient", backref="fitness_expert")  
 
-  def __init__(self, name, working_hours=0, salary=0.0):
-    self.name = name
+  def __init__(self,id,firstname, lastname, gender,age,address,email, phone_number,salary,working_hours=0):
+    self.id = None  
+    self.firstname = firstname 
+    self.lastname = lastname
+    self.age = age
+    self.gender = gender
+    self.address =address
+    self.email= email
+    self.phone_number = phone_number
     self.working_hours = working_hours
-    self.salary = salary
+    self.salary = salary 
+    self.calculate_salary()
+
+  def calculate_salary(self,working_hours,salary):
+        #Calculating fitness_expert's salary based on his/her working hours since fitness experts will be working on consulatation basis 
+    salary = (500 * working_hours)
+
+  def __str__(self):
+   #template for printing doctors details 
+       return f"fitness Expert:ID: {self.id}, {self.first_name} {self.last_name} (ID: {self.id}) - Age: {self.age}, Gender: {self.gender},Address:{self.address}, Email: {self.email} , Phone_number: {self.phone_number}, Working Hours: {self.working_hours}, Salary: ${self.salary:.2f}"
